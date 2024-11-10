6133331b39e450dd893129a0083fad475f6adf66
Nosebones69 <ghostsoldier222@gmail.com>
2024-11-09 19:17:37 -0500

    ja

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,8 @@
-import { db } from "$lib/server/database/client";
+/*import { db } from "$lib/server/database/client";
  import { estudiantes } from "$lib/server/database/schema";
 
 export const load= async()=>{
     let Tabla = await db.select().from(estudiantes) 
     console.log(estudiantes)
     return {estudiantes}
-}
+}*/
